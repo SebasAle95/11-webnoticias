81a58d22fab8295ecaabbbb36b6199b2a24d8520
Sebastian Orosco <sebas_ale05@hotmail.com>
2023-06-04 23:22:26 -0300

    maquetado de noticias

---
+++ src/components/Noticias.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+
+const Noticias = () => {
+    return (
+        <>
+            <Form.Select aria-label="Default select example">
+      <option>Seleccione una seccion de noticias</option>
+      <option value="1">Politica</option>
+      <option value="2">Deporte</option>
+      <option value="3">Policiales</option>
+    </Form.Select>
+        </>
+    );
+};
+
+export default Noticias;
