e70b60b442a332ccd82f4e812d36fe38c0ea40ec
Sebastian Orosco <sebas_ale05@hotmail.com>
2023-06-05 23:44:07 -0300

    logicaterminada

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,28 @@ import { Container } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Noticias from './components/Noticias';
 import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  
+  const [noticias, setNoticias] = useState({});
+
+  useEffect(()=>{
+    consultarNoticias();
+  }, []);
+
+  const consultarNoticias = async ()=>{
+
+    try {
+
+    const respuesta = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-05-06&sortBy=publishedAt&apiKey=a29872ae476341faa216cd80191011d9');
+    const dato =  await respuesta.json();
+    console.log(respuesta);
+    console.log(dato);
+      setNoticias(dato);
+    }catch(error){
+      console.log(error);
+    }
+  }
 
   return (
     <>
@@ -13,7 +32,7 @@ function App() {
         <h1 className='text-center display-4'>Noticias</h1>
         <hr/>
         </section>
-        <Noticias></Noticias>
+        <Noticias noticias={noticias} />
       </Container>
     </>
   )
--- src/components/Noticias.jsx
+++ src/components/Noticias.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 
-const Noticias = () => {
+const Noticias = (noticias) => {
     return (
         <>
             <Form.Select aria-label="Default select example">
       <option>Seleccione una seccion de noticias</option>
-      <option value="1">Politica</option>
+      <option value="1">Noticias</option>
       <option value="2">Deporte</option>
       <option value="3">Policiales</option>
     </Form.Select>
