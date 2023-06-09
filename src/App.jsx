1e9201f23674b8315b3a2cbb19880cbdfa8f107a
Sebastian Orosco <sebas_ale05@hotmail.com>
2023-06-09 20:56:39 -0300

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { Container } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Noticias from './components/Noticias';
+import ListaNoticias from './components/ListaNoticias';
 import './App.css'
 import { useState, useEffect } from "react";
 
 function App() {
   const [noticias, setNoticias] = useState({});
+  const [buscar, setBuscar] = useState({})
+
 
   useEffect(()=>{
     consultarNoticias();
-  }, []);
+  }, [buscar]);
 
   const consultarNoticias = async ()=>{
 
     try {
 
-    const respuesta = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-05-06&sortBy=publishedAt&apiKey=a29872ae476341faa216cd80191011d9');
+    const respuesta = await fetch(`https://newsapi.org/v2/everything?q=${buscar}&apiKey=a29872ae476341faa216cd80191011d9`);
     const dato =  await respuesta.json();
+    
     console.log(respuesta);
     console.log(dato);
       setNoticias(dato);
@@ -24,7 +28,10 @@ function App() {
       console.log(error);
     }
   }
-
+  const onHandleChange = (value)=>{
+    console.log(value)
+    setBuscar(value)
+  }
   return (
     <>
       <Container className='my-5 main'>
@@ -32,7 +39,8 @@ function App() {
         <h1 className='text-center display-4'>Noticias</h1>
         <hr/>
         </section>
-        <Noticias noticias={noticias} />
+        <Noticias noticias={noticias} onHandleChange={onHandleChange} />
+        {noticias.articles ? <ListaNoticias lista={noticias.articles}/>: 'no hay articulo'}
       </Container>
     </>
   )
--- src/components/Noticias.jsx
+++ src/components/Noticias.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
+import ListaNoticias from './ListaNoticias';
 
-const Noticias = (noticias) => {
+const Noticias = ({onHandleChange}) => {
     return (
         <>
-            <Form.Select aria-label="Default select example">
+            <Form.Select onChange={(e)=>  onHandleChange(e.target.value)} aria-label="Default select example">
+                
       <option>Seleccione una seccion de noticias</option>
-      <option value="1">Noticias</option>
-      <option value="2">Deporte</option>
-      <option value="3">Policiales</option>
+      <option value="mundo">Mundo</option>
+      <option value="deporte">Deporte</option>
+      <option value="policiales">Policiales</option>
+      
     </Form.Select>
         </>
     );
